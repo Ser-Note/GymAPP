5b7d43074790fe8d7181f0626477011058019e68
Ser-Note <ITProgramming@berkscareer.com>
2026-01-21 20:00:30 -0500

    added css for phone

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(session({
     cookie: {
         secure: isProd,
         httpOnly: true,
-        sameSite: 'none',
+        sameSite: isProd ? 'none' : 'lax',
         maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
 }));
